390caf6f2ba0ef27c268196bccf2340bf3c6f806
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2025-09-22 10:47:37 -0100

    fix: bug getting the absolute path for build driectory

--- tests/build.bat
+++ tests/build.bat
@@ -7,10 +7,11 @@ FOR %%G IN ("%~dp0..") DO SET "root_dir=%%~fG"
 SET build_folder_name=build
 SET build_dir=%current_dir%%build_folder_name%
 
-@REM ECHO ROOT_DIR:    %root_dir%
-@REM ECHO CURRENT_DIR: %current_dir%
+ECHO ROOT_DIR:    %root_dir%
+ECHO CURRENT_DIR: %current_dir%
+ECHO BUILD_DIR:   %build_dir%
 
-IF NOT EXIST %build_dir% MKDIR %build_folder_name%
+IF NOT EXIST %build_dir% MKDIR %build_dir%
 PUSHD %build_dir%
 cl /TC /Zi /I "%root_dir%" ../tests.c ../t_dynamic_array.c ../t_linked_list.c ../t_queue.c ../t_stack.c /link /nologo /out:tests.exe
 @REM link main.obj /out:main.exe
